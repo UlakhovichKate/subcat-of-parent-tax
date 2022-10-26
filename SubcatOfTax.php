79546817f2c7e6a9bf25b446dcaa86cd53d56a0c
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2022-10-26 15:14:06 +0300

    first commit

---
+++ SubcatOfTax.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+Plugin Name: Subcategories of Parent Terms
+Plugin URI:
+Description: Grid of subcategories of all parent terms of a custom taxonomy
+Version: 1.0.0
+Author: Katsiaryna Ulakhovich
+Author URI: https://github.com/UlakhovichKate
+License: GPLv2 or later
+Text Domain: subcat-of-parent-tax-plugin
+*/
+
+defined( 'ABSPATH' ) || exit;
+
+if(!class_exists('SubcatOfTax') ) {
+
+    class SubcatOfTax {
+
+        function register() {
+
+            require_once plugin_dir_path(__FILE__) . 'includes/getSubcategories.php';
+            $this->add_actions();
+
+        }
+
+        function add_actions() {
+            add_action( 'wp_enqueue_scripts',  [__CLASS__, 'enqueue_styles'] );
+        }
+
+        function enqueue_styles() {
+            wp_enqueue_style( 'subcat-of-tax-styles',  plugin_dir_url( __FILE__ ) . 'style.css' );
+        }
+    }
+
+    $plugin = new SubcatOfTax();
+    $plugin->register();
+}
---
+++ includes/getSubcategories.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Get the list subcategories of added taxonomy
+ * */
+
+function getSubcategories() {
+    $taxonomies = get_terms( array(
+        'taxonomy' => 'education_cat', // custom taxonomy
+        'hide_empty' => false,
+        'order_by' => 'name'
+    ) );
+
+    if ( !empty($taxonomies) ) :
+        $output = '<div class="plugin">';
+
+        foreach( $taxonomies as $category ) {
+
+            if( $category->parent == 0 ) {
+
+                $output .= '<ul class="list" data-parent="'. esc_attr( $category->name ) . '">';
+
+                foreach( $taxonomies as $subcategory ) {
+
+                    if( $subcategory->parent == $category->term_id ) {
+
+                        $image = get_field('img', $subcategory);
+                        $order = get_field('order', $subcategory);
+
+                        $output.= '<li class="item order-'. $order .'" data-id="'. esc_attr( $subcategory->term_id ) .'"><div class="image-wrapper"><img class="image" src="'. $image .'" alt="'. $subcategory->name .'"></div><span class="name">'. esc_html($subcategory->name) .'</span></li>';
+                    }
+
+                }
+
+                $output .= '</ul>';
+
+            }
+        }
+        $output .= '</div>';
+
+        return $output;
+
+    endif;
+}
+
+add_action( 'init', 'getSubcategories' );
+add_shortcode( 'subcatoftax', 'getSubcategories' );
